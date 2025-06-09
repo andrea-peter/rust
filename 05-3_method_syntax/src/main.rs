a0fa50c01fbf4e3340f28a451cca54ef807c093a
Andrea Peter <pedrudehuere@hotmail.com>
2025-06-09 12:32:05 +0200

    Add method_syntax

---
+++ 05-3_method_syntax/src/main.rs
@@ -0,0 +1,50 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+// Multiple `impl` blocks per struct are allowed
+impl Rectangle {
+    // We can choose whether we borrow or own self, mutable or not
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    // Methods can have the same name as fields
+    fn width(&self) -> bool {
+        // Pretty bad design =(
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    if rect1.width() {
+        println!("The rectangle has a nonzero width, it is: {}", rect1.width);
+    }
+    println!("The are of rect1 is {} square pixels", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 40,
+        height: 60,
+    };
+
+    let rect3 = Rectangle::square(100);
+
+    println!("Can rect1 hold rect2?: {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect3?: {}", rect2.can_hold(&rect3));
+}
